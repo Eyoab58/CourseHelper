007b1543d2d462e44afcc5d07b73f2aa64921b89
Eyoab Asrat <eyoabasrat@gmail.com>
2021-08-15 18:05:32 -0400

    Added ClassTableViewCell

---
+++ CourseHelper/classes.swift
@@ -0,0 +1,13 @@
+//
+//  classes.swift
+//  CourseHelper
+//
+//  Created by Eyoab Asrat on 6/17/21.
+//
+
+import Foundation
+
+struct classes: Codable {
+    var course_id: String
+    var name: String
+}
